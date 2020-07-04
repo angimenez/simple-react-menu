b16ad8e0112c648beacb0f311454ef69f02396c1
angimenez <angimenez1996@gmail.com>
2020-07-03 22:49:34 -0300

    Prueba

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
-import resolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: "src/MenuContainer.js",
@@ -9,6 +10,7 @@ export default {
   },
   plugins: [
     resolve(),
-    babel({ babelHelpers: 'bundled' })
+    babel({ babelHelpers: "bundled" }),
+    commonjs(),
   ],
 };
